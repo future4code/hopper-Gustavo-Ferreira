20dbf7a9e93cca2ab9f7eda635c14873d2a8fa53
Gustavogaf <gustavoaferreiragt2010@gmail.com>
2022-02-08 21:36:46 -0300

    atividade de variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,59 @@
+//let a = 10
+//let b = 10
+
+//console.log(b)
+
+//b = 5
+//console.log(a, b)
+
+// ira aparecer no consolo :
+//10
+//10,5
+
+//let a = 10
+//let b = 20
+//c = a
+//b = c
+//a = b
+
+//console.log(a, b, c)
+
+// ira aparecer no console 10, 10, 10
+
+
+//let p = prompt("Quantas horas você trabalha por dia?")
+//let t = prompt("Quanto você recebe por dia?")
+//alert(`Voce recebe ${t/p} por hora`)
+
+
+// para "p" eu colocaria "jornadadiaria" e "t" colocaria "recebe por dia"
+
+let nome 
+let idade
+ typeof nome
+ typeof idade
+
+ //no console foi exibido que a variavel é undefined provavelmente por que não atribui nada a elas
+
+ nome = prompt("Qual o seu nome?")
+idade = prompt("Quantos anos vc tem?")
+typeof nome
+typeof idade
+
+//ele retornou que as duas respostas são "string"
+
+const diadasemana = prompt("Hoje é terça feira?")
+const suaidade = prompt ("você tem mais de 21 anos?")
+const primeiraletra = prompt ("seu nome começa com a letra A°")
+
+console.log (diadasemana,suaidade,primeiraletra)
+
+//.
+
+let a = 10
+let b = 25
+c = a
+a = b
+b = c
+
+console.log(a, b)
