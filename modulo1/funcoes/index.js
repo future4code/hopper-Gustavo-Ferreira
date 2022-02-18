87bb582d978ac5b1d41e5d4037d998197ed9453e
Gustavogaf <gustavoaferreiragt2010@gmail.com>
2022-02-17 21:45:04 -0300

    atividade de funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,87 @@
+//exercicio 1
+
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// minhaFuncao(2)
+// minhaFuncao(10)
+
+//o primeiro console.log retornaria "10" e o segundo retornaria "50"
+//o console não retornaria nada
+
+//exercicio 2
+
+// let textoDoUsuario = prompt("Insira um texto");
+
+// const outraFuncao = function(texto) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+//essa função recebe uma variavel com uma string, deixa todas as letras em minusculo e retorna treu ou false caso tenha
+// "cenoura" dentro da variavel
+
+//1º eu gosto de cenura, true\2º cenoura é bom para a vista, true\3º cenouras crescem na terra, false.
+
+//exercicio 3
+
+// function mensagem (){"Eu sou Gustavo, tenho 21 anos, moro em Lagarto e sou estudante."}
+
+// console.log(mensagem)
+
+
+// let seuNome = prompt("Qual o seu nome?")
+// let suaIdade = prompt("Qual a sua idade?")
+// let suaCidade = prompt("Em qual cidade você mora?")
+// let suaProfissao = prompt("Qual a sua profissão?")
+
+
+// function aprensentacao (nome, idade, cidade, profissão){return`Eu sou ${nome}, tenho ${idade} anos, 
+// moro na cidade de ${cidade} e trabalho como ${profissão}`}
+
+// console.log (aprensentacao(seuNome, suaIdade, suaCidade, suaProfissao))
+
+//exercicio 4
+
+// function soma (primeiro, segundo){return primeiro+segundo}
+
+// console.log (soma(5,5))
+
+// function maiorOuIgual (primeiro, segundo){return primeiro >= segundo}
+
+// console.log (maiorOuIgual(2,5))
+
+// function parOuImpar (number){return (number%2) == 0}
+
+// console.log(parOuImpar(10))
+
+// function texto (mensagem) {return console.log(mensagem.length) console.log(mensagem.touppercase())  }
+
+// console.log(texto("novo"))
+
+//exercicio 5
+
+// let primeiro = prompt ("insira o primeiro numero")
+
+// let segundo = prompt ("insira o segundo numero")
+
+// Number(primeiro)
+// Number(segundo)
+
+
+
+// function soma (primeiro, segundo){return primeiro+segundo}
+
+// function divisão (primeiro, segundo){return primeiro/segundo}
+
+// function subtracao (primeiro, segundo){return primeiro-segundo}
+
+// function multi (primeiro, segundo){return primeiro*segundo}
+
+// console.log (soma(primeiro,segundo))
+// console.log (subtracao(primeiro,segundo))
+// console.log (multi(primeiro,segundo))
+// console.log (divisão(primeiro,segundo))
