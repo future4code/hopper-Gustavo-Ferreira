b01297e327102d93c2a9e4e16f95589a2ace69a0
Gustavogaf <gustavoaferreiragt2010@gmail.com>
2022-03-13 22:47:18 -0300

    1ª parte finalizada

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -10,3 +10,29 @@
  * 
  * 
  */
+
+   //  console.log("Boas vindas ao jogo Blackjack!")
+
+   // const carta1 = comprarCarta()
+   // const carta2 = comprarCarta()
+   // const carta3 = comprarCarta()
+   // const carta4 = comprarCarta()
+
+
+   // const usuario = carta1.valor + carta2.valor
+   // const computador = carta3.valor + carta4.valor 
+
+   // let inicio = confirm ("Quer iniciar uma nova rodada?")
+   
+   // if (inicio === true) {
+   //    console.log(`Usuário - cartas: ${carta1.texto} ${carta2.texto} - pontuação ${usuario}`)
+   //    console.log(`Computador - cartas: ${carta3.texto} ${carta4.texto} - pontuação ${computador}`)
+   // }
+
+   // if (usuario > computador) {
+   //    console.log("Usuário ganhou")
+   // } else if (usuario < computador){
+   // console.log("Computador ganhou")
+   // } else{
+   //    console.log("Empate")
+   // }
