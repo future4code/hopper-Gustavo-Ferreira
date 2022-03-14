647f662f278dcb77bbaa93a7027b17e5df6f62a2
Gustavogaf <gustavoaferreiragt2010@gmail.com>
2022-03-14 18:59:25 -0300

    projeto de fixação final

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -11,5 +11,61 @@
  * 
  */
 
-
+let novaPartida = confirm("Bem vindes ao jogo Blackjack!"+ '\n' +"Quer começar uma nova partida?")
     
+
+// const sorteioUm = {
+//    cartaUm: [comprarCarta().texto, comprarCarta().valor],
+//    cartaDois: [comprarCarta().texto, comprarCarta().valor],
+//    cartaTres: [comprarCarta().texto, comprarCarta().valor],
+//    cartaQuatro: [comprarCarta().texto, comprarCarta().valor],
+//    cartaCinco: [comprarCarta().texto, comprarCarta().valor],
+//    cartaSeis: [comprarCarta().texto, comprarCarta().valor],
+//    cartaSete: [comprarCarta().texto, comprarCarta().valor],
+//    cartaOito: [comprarCarta().texto, comprarCarta().valor],
+//    cartaNove: [comprarCarta().texto, comprarCarta().valor],
+//    cartaDez: [comprarCarta().texto, comprarCarta().valor],
+// }
+
+// const sorteiodois = {
+//    cartaUm: [comprarCarta().texto, comprarCarta().valor],
+//    cartaDois: [comprarCarta().texto, comprarCarta().valor],
+//    cartaTres: [comprarCarta().texto, comprarCarta().valor],
+//    cartaQuatro: [comprarCarta().texto, comprarCarta().valor],
+//    cartaCinco: [comprarCarta().texto, comprarCarta().valor],
+//    cartaSeis: [comprarCarta().texto, comprarCarta().valor],
+//    cartaSete: [comprarCarta().texto, comprarCarta().valor],
+//    cartaOito: [comprarCarta().texto, comprarCarta().valor],
+//    cartaNove: [comprarCarta().texto, comprarCarta().valor],
+//    cartaDez: [comprarCarta().texto, comprarCarta().valor],
+// }
+
+let cartasUsuario = []
+let cartasComputador = []
+let valoresUsuario = []
+let valoresComputador = []
+
+if (novaPartida === true){
+   let a = compraCarta()
+   let b = compraCarta()
+   let c = compraCarta()
+   let d = compraCarta()
+
+   let primeiraRodada = confirm(`Suas cartas são ${a} ${b}.
+   A carta revelada do computador é ${c}`)
+    
+   cartasUsuario.push(a,b)
+   cartasComputador.push(c,d)
+
+   valoresUsuario.push(sorteioUm.cartaUm[1],sorteioUm.cartaDois[1])
+   valoresComputador.push(sorteiodois.cartaUm[1],sorteiodois.cartaDois[1] )
+
+   } else {
+      alert(`O jogo acabou!`)
+   }
+
+   console.log(cartasUsuario)
+   console.log(cartasComputador)
+   console.log(valoresComputador)
+   console.log(valoresUsuario)
+
