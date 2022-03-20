e5a6a4e3747db9eb51e49977c7f57238e6e6c295
Gustavogaf <gustavoaferreiragt2010@gmail.com>
2022-03-20 12:49:24 -0300

    exercicio 12

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -135,7 +135,9 @@ return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${f
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
    
+    const pessoaAnonima = {...pessoa, nome: "ANÔNIMO"}
     
+    return pessoaAnonima
 }
 
 // EXERCÍCIO 13A
