ff274acc7c4dee38ba8b65fe3a2c93ab7b17da80
Gustavogaf <gustavoaferreiragt2010@gmail.com>
2022-05-27 22:20:35 -0300

    inicio do projeto labefy

---
+++ modulo3/projeto-labefy/projeto-labefy/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+import Listadeplaylist from './Components/Listadeplaylist';
+import Musicasdaplaylist from './Components/Musicasdaplaylist';
+
+
+class App extends React.Component {
+  
+  
+  
+  render(){
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}}
+
+export default App;
---
+++ modulo3/projeto-labefy/projeto-labefy/src/Components/Listadeplaylist.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Listadeplaylist extends React.Component{
+
+
+
+    render(){
+        return(
+            <div>
+
+
+            </div>
+        )
+    }
+} export default Listadeplaylist
